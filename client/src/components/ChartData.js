161333eae057982d6f64a1f163ae9506524e385f
khaynes-coop <k.haynes3238@gmail.com>
2020-12-06 03:04:01 -0500

    Ughhh Almost

--- client/src/components/ChartData.js
+++ client/src/components/ChartData.js
@@ -2,11 +2,14 @@ import Order_Data from '../Order_Data.json';
 import Subscription_Data from '../Subscription_Data.json';
 var MonthDays = []; 
 var SubsMonthDays = [];
+var hoursDay = [];
+var calculationHoursDay = [];
+const today = new Date(2014, 0, 30);
+//Calculates and stores the year, month, and day
 for(var i = 0; i < 30; i++){
-    const today = new Date(2020, 6, 30);
     var dd = today.getDate() - 29 + i;
     var yyyy = today.getFullYear();
-    if(dd <= 0 ){
+    if(dd <= 0 ){ //if the day is before the current month calculates the correct day
         var mm = today.getMonth();
         if(mm == 0 ){
             mm = 12;
@@ -22,26 +25,50 @@ for(var i = 0; i < 30; i++){
     else{
     var mm = (today.getMonth() + 1);}
     var S = mm + '-' + dd + '-'+ yyyy;
-    //console.log(D);
     MonthDays.push(S);
     SubsMonthDays.push(S);
 }
+
+
+    var ddd = today.getDate();
+    var mmm = today.getMonth();
+    var yyy = today.getFullYear();
+for(var i = 0; i < 24; i++){
+    var hhh = today.getHours() - 23 + i;
+    if(hhh <= 0){
+        hhh = hhh + 23;
+        var D = (ddd - 1) + '-' + hhh;
+        hoursDay.push(D)
+        if(hhh > 12){var H = (ddd - 1) + '-' + (hhh - 12) + "PM";
+        calculationHoursDay.push(H);}
+        else{ var H = (ddd - 1) + '-' + (hhh) + "AM";
+            calculationHoursDay.push(H);}
+    }
+    else{
+        var D = ddd + '-' + hhh;
+        hoursDay.push(D)
+        if(hhh > 12){var H = (ddd) + '-' + (hhh - 12) + "PM";
+        calculationHoursDay.push(H);}
+        else{ var H = (ddd) + '-' + (hhh) + "AM";
+            calculationHoursDay.push(H);}
+    }
+}
+
+
 var weekDays = [];
 var SubsweekDays = [];
 for (var i = 0; i < 7; i++){
     weekDays[i] = MonthDays[23 + i];
     SubsweekDays[i] = SubsMonthDays[23 + i];
 }
+
+
 var YearDays = [];
-for(var j = 0; j < 12; j++){
-    const today = new Date();
-    var mm = (today.getMonth() + 1 -11 + j);
-    if (mm <= 0){
-        mm = mm + 12;
-    }
-    var D = mm + '/1'; 
-    YearDays.push(D);
+for(var j = 2010; j <= yyy; j++){ //put start year here
+    YearDays.push(j.toString());
 }
+
+
 var OrderDataMonth = Array(MonthDays.length).fill(0);
 var PoundDataMonthCompounded = Array(MonthDays.length).fill(0);
 var PoundDataMonth = Array(MonthDays.length).fill(0);
@@ -88,13 +115,19 @@ for (var c = 0; c < 7; c++){
     PoundDataWeek[c] = (PoundDataMonth[23 + c]);
 }
 var OrderDataDefault = [];
+var PoundDataDefault = [];
 for (var c = 0; c < 7; c++){
-    OrderDataDefault[c] = (OrderDataMonth[23 + c]);
+    OrderDataDefault[c] = (OrderDataWeek[c]);
+    PoundDataDefault[c] = (PoundDataWeek[c]);
 }
 
 
 var SubsDataMonthCompunded = Array(SubsMonthDays.length).fill(0);
 var SubsDataMonth = Array(SubsMonthDays.length).fill(0);
+var SubsFamilyData = Array(SubsMonthDays.length).fill(0);
+var SubsStandardData = Array(SubsMonthDays.length).fill(0);
+var SubsStudentData = Array(SubsMonthDays.length).fill(0);
+var SubsPlusData = Array(SubsMonthDays.length).fill(0);
 for (var j = 0; j < SubsMonthDays.length; j++){ 
     var month =  SubsMonthDays[j];
     var first =  month.indexOf('-');
@@ -103,8 +136,24 @@ for (var j = 0; j < SubsMonthDays.length; j++){
     var monthday = month.substr(first+ 1, second -3);
     var monthyear = month.substr(second+1);
     for (var i = 0; i < Subscription_Data.length; i++){
-        if(SubsMonthDays[j] === Subscription_Data[i].startDate)
-        {SubsDataMonth[j] =SubsDataMonth[j] + 1;}
+        if(SubsMonthDays[j] === Subscription_Data[i].startDate){
+            if(Subscription_Data[i].subscriptionType == "family"){
+                SubsFamilyData[j] = SubsFamilyData[j] + 1;
+                SubsDataMonth[j] =SubsDataMonth[j] + 1;
+            }
+            else if(Subscription_Data[i].subscriptionType == "standard"){
+                SubsStandardData[j] = SubsStandardData[j] + 1;
+                SubsDataMonth[j] =SubsDataMonth[j] + 1;
+            }
+            else if(Subscription_Data[i].subscriptionType == "plus"){
+                SubsPlusData[j] = SubsPlusData[j] + 1;
+                SubsDataMonth[j] =SubsDataMonth[j] + 1;
+            }
+            else if(Subscription_Data[i].subscriptionType == "student"){
+                SubsStudentData[j] = SubsStudentData[j] + 1;
+                SubsDataMonth[j] =SubsDataMonth[j] + 1;
+            }
+        }
         var string = Subscription_Data[i].startDate;
         var firstS =  string.indexOf('-');
         var stringmonth = string.substr(0, firstS);
@@ -126,12 +175,53 @@ for (var j = 0; j < SubsMonthDays.length; j++){
     }
 }
 
-var SubsDataWeekCompunded = [];
+var YearOrderData = Array(YearDays.length).fill(0);
+var YearPoundData = Array(YearDays.length).fill(0);
+for(var i = 0; i < YearDays.length; i++){
+    for(var j = 0; j < Order_Data.length; j++){
+        if(YearDays[i] === Order_Data[j].orderDate.substr(Order_Data[j].orderDate.length - 4)){
+            YearOrderData[i] = YearOrderData[i] + 1;
+            YearPoundData[i] = YearPoundData[i] + Order_Data[j].orderWeight;
+        }
+    }
+}
+
+
+var PoundDataDay =  Array(calculationHoursDay.length).fill(0);
+var currDay =  mmm+1 + '-' + ddd + '-' + yyy;
+var dash = currDay.indexOf('-');
+var dash2 = currDay.indexOf('-', dash + 1);
+var OrderDataDay = Array(calculationHoursDay.length).fill(0);
+for(var i = 0; i < calculationHoursDay.length; i++){
+    var dashs =calculationHoursDay[i].indexOf('-');
+    var hrs = calculationHoursDay[i].substr(dashs + 1)
+if(currDay.substr(dash, dash2) !== calculationHoursDay[i].substr(0, dashs)){
+    currDay = currDay.substr(0, dash + 1) + calculationHoursDay[i].substr(0, dashs) + currDay.substr(dash2);
+}
+    for(var j = 0; j < Order_Data.length; j++){
+        if(Order_Data[j].orderDate === currDay){
+            var tme = Order_Data[j].deliveryTime.substr(0, Order_Data[j].deliveryTime.indexOf(':')) + Order_Data[j].deliveryTime.substr(Order_Data[j].deliveryTime.length - 2);
+            if(hrs === tme){
+                OrderDataDay[i] = OrderDataDay[i] + 1;
+                PoundDataDay[i] = PoundDataDay[i] + Order_Data[j].orderWeight;
+            }        
+        }
+    }
+}
+
+
 var SubsDataWeek = [];
+var PlusDataWeek = [];
+var FamilyDataWeek = [];
+var StandardDataWeek = [];
+var StudentDataWeek = [];
 for (var c = 0; c < 7; c++){
-    SubsDataWeekCompunded[c] = (SubsDataMonthCompunded[23 + c]);
     SubsDataWeek[c] = (SubsDataMonth[23 + c]);
+    PlusDataWeek[c] = SubsPlusData[23 + c];
+    StudentDataWeek[c] = SubsStudentData[23 + c];
+    FamilyDataWeek[c] = SubsFamilyData[23 + c];
+    StandardDataWeek[c] = SubsStandardData[23 + c];
 }
 
 export default MonthDays;
-export {weekDays, YearDays, PoundDataMonth, PoundDataWeek,  OrderDataMonth, OrderDataWeek, OrderDataDefault, SubsDataMonthCompunded, SubsMonthDays, SubsDataWeekCompunded, SubsDataWeek, SubsweekDays, SubsDataMonth, PoundDataMonthCompounded, PoundDataWeekCompounded };
+export {StudentDataWeek, FamilyDataWeek, StandardDataWeek, PlusDataWeek, calculationHoursDay, weekDays, YearOrderData, YearPoundData, YearDays, OrderDataDay, PoundDataMonth, PoundDataWeek, OrderDataMonth, OrderDataWeek, OrderDataDefault, PoundDataDefault, SubsDataMonthCompunded, SubsMonthDays, SubsDataWeek, SubsweekDays, SubsDataMonth, PoundDataMonthCompounded, PoundDataWeekCompounded, PoundDataDay, SubsFamilyData, SubsPlusData, SubsStandardData, SubsStudentData };
--- client/src/components/SubsChart.js
+++ client/src/components/SubsChart.js
@@ -1,6 +1,6 @@
 import React, {Component, useEffect, useState} from 'react';
 import {Line} from 'react-chartjs-2';
-import  { YearDays, SubsDataMonth, SubsMonthDays, SubsDataWeek, SubsweekDays} from './ChartData';
+import  { YearDays, SubsDataMonth, SubsMonthDays, SubsDataWeek, SubsweekDays, SubsFamilyData, SubsPlusData, SubsStandardData, SubsStudentData} from './ChartData';
 
 
 var MonthData = SubsDataMonth;
@@ -9,6 +9,10 @@ var WeekData = SubsDataWeek;
 var weekLabels = SubsweekDays;
 var monthLabels = SubsMonthDays;
 var defaultLabels = SubsweekDays;
+var StudentData = SubsStudentData;
+var StandardData = SubsStudentData;
+var FamilyData = SubsStudentData;
+var PlusData = SubsStudentData;
 
 
 var RandomOrg = require('random-org');
@@ -17,7 +21,7 @@ const SubsChart = () =>{
 const [chartData, setChartData] = useState({});
     let default_color  = 'rgba(1, 201, 225, 1)';
 
-    var random = new RandomOrg({ apiKey: '15aa22b5-8b99-444f-9287-4e704cfe66b0' });
+    /*var random = new RandomOrg({ apiKey: '15aa22b5-8b99-444f-9287-4e704cfe66b0' });
     let maybe = random.generateIntegers({ min: 0, max: 255, n: 3 })
         .then(function(result) {
             console.log(result.random.data); // [55, 3]
@@ -26,14 +30,16 @@ const [chartData, setChartData] = useState({});
     let random_color = random.generateIntegers({ min: 0, max: 255, n: 3 });
     console.log(random);
     console.log(maybe);
-    console.log(random_color);
+    console.log(random_color);*/
 
     const SubsChart = () =>{
         setChartData({
             labels:defaultLabels, 
             datasets: [{
                     data: DefaultData,
+                    label: "All New Subscribers",
                     backgroundColor:['rgba(54, 162, 235, 0.0)'],
+                    borderColor: default_color,
                 }
             ]
         })
