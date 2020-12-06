a06a4066537a8c5186e753025e603862d0144584
khaynes-coop <k.haynes3238@gmail.com>
2020-12-06 02:53:13 -0500

    Should be complete for Orders and pounds

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -1,15 +1,17 @@
 import React, {Component, useEffect, useState} from 'react';
 import {Line} from 'react-chartjs-2';
-import{SubsMonthDays, SubsweekDays, YearDays, OrderDataMonth, OrderDataWeek, OrderDataDefault} from './ChartData';
+import{calculationHoursDay, SubsMonthDays, SubsweekDays, YearOrderData, YearDays, OrderDataDay, OrderDataMonth, OrderDataWeek, OrderDataDefault} from './ChartData';
 
-
+var hoursData = OrderDataDay;
+var hoursLabels = calculationHoursDay;
 var MonthData = OrderDataMonth;
 var DefaultData = OrderDataDefault; 
 var WeekData = OrderDataWeek;
 var weekLabels = SubsweekDays;
 var monthLabels = SubsMonthDays;
 var defaultLabels = SubsweekDays;
-
+var YearData = YearOrderData;
+var YearLabels = YearDays;
 
 var RandomOrg = require('random-org');
 
@@ -17,7 +19,7 @@ const Chart = () =>{
 const [chartData, setChartData] = useState({});
     let default_color  = 'rgba(1, 201, 225, 1)';
 
-    var random = new RandomOrg({ apiKey: '15aa22b5-8b99-444f-9287-4e704cfe66b0' });
+    /*var random = new RandomOrg({ apiKey: '15aa22b5-8b99-444f-9287-4e704cfe66b0' });
     let maybe = random.generateIntegers({ min: 0, max: 255, n: 3 })
         .then(function(result) {
             console.log(result.random.data); // [55, 3]
@@ -26,7 +28,7 @@ const [chartData, setChartData] = useState({});
     let random_color = random.generateIntegers({ min: 0, max: 255, n: 3 });
     console.log(random);
     console.log(maybe);
-    console.log(random_color);
+    console.log(random_color);*/
 
     const chart = () =>{
         setChartData({
@@ -34,6 +36,7 @@ const [chartData, setChartData] = useState({});
             datasets: [{
                     data: DefaultData,
                     backgroundColor:['rgba(54, 162, 235, 0.0)'],
+                    borderColor: default_color,
                 }
             ]
         })
@@ -47,6 +50,7 @@ const [chartData, setChartData] = useState({});
             datasets: [{
                     data: MonthData,
                     backgroundColor:['rgba(54, 162, 235, 0.0)'],
+                    borderColor: default_color,
                 }
             ]
         })
@@ -58,6 +62,29 @@ const [chartData, setChartData] = useState({});
             datasets: [{
                     data: WeekData,
                     backgroundColor:['rgba(54, 162, 235, 0.0)'],
+                    borderColor: default_color,
+                }
+            ]
+        })
+    }
+    function updateChartHours(chart) {
+        setChartData({
+            labels:hoursLabels, 
+            datasets: [{
+                    data: hoursData,
+                    backgroundColor:['rgba(54, 162, 235, 0.0)'],
+                    borderColor: default_color,
+                }
+            ]
+        })
+    }
+    function updateChartYears(chart) {
+        setChartData({
+            labels:YearLabels, 
+            datasets: [{
+                    data: YearData,
+                    backgroundColor:['rgba(54, 162, 235, 0.0)'],
+                    borderColor: default_color,
                 }
             ]
         })
@@ -67,8 +94,10 @@ return(
     <div>
     <div>
         <Line data = {chartData} options ={{title:{text: 'New Orders', fontFamily: 'Calmer', fontSize: 25, display: true}}}></Line>
+        <button onClick = {updateChartHours}>Hours</button>
         <button onClick = {updateChart}>Week</button>
         <button onClick = {updateChartMonth}>Month</button>
+        <button onClick = {updateChartYears}>All Time</button>
         </div>
     </div>
 )
